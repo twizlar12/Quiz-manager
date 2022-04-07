26833829e226fc2baeefbe26b4c2d09b7dce7821
Kabir Qazi <kqazi2@ford.com>
2022-04-07 16:58:57 +0100

    Can access login and register page

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const app = express()
 app.set('view-engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', { name: 'WebbiSkools' })
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
 })
 
 app.listen(3000)
